77566952ff4a3bc2c01a433384a84e48e16db8bc
Vincentius <vincentiussss@gmail.com>
2025-03-17 19:27:12 +0700

    exclude image from article card

--- src/app/components/ArticleCard/index.tsx
+++ src/app/components/ArticleCard/index.tsx
@@ -1,38 +1,20 @@
-import Image from "next/image";
 import { FC } from "react";
 
 interface ArticleCardType {
   title: string;
   description: string;
-  imgSrc: string;
-  imgAlt: string;
 }
-const ArticleCard: FC<ArticleCardType> = ({
-  title,
-  description,
-  imgSrc,
-  imgAlt,
-}) => {
+
+const ArticleCard: FC<ArticleCardType> = ({ title, description }) => {
   return (
-    <div className="flex flex-col md:flex-row-reverse gap-4">
-      <div className="flex md:w-1/4">
-        <Image
-          height={100}
-          width={200}
-          src={imgSrc}
-          alt={imgAlt}
-          className="object-cover object-top w-full h-[536px] md:h-[359px] rounded-2xl"
-        />
+    <div className="flex flex-col md:w-3/4 rounded-2xl bg-[#FFFFFE] py-3 md:py-6 md:px-8 px-6 gap-5 border-2 border-[var(--border-grey)]">
+      <div className="text-[var(--text-default)] text-4xl font-bold">
+        {title}
       </div>
-      <div className="flex flex-col md:w-3/4 rounded-2xl bg-[#FFFFFE] py-3 md:py-6 md:px-8 px-6 gap-5 border-2 border-[var(--border-grey)]">
-        <div className="text-[var(--text-default)] text-4xl font-bold">
-          {title}
-        </div>
-        <div
-          className="text-[var(--text-secondary)] text-xl"
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></div>
-      </div>
+      <div
+        className="text-[var(--text-secondary)] text-xl"
+        dangerouslySetInnerHTML={{ __html: description }}
+      ></div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ArticleCard from "./components/ArticleCard";
 import Navbar from "./components/Navbar";
 import PageHeadline from "./components/PageHeadline";
@@ -11,12 +12,21 @@ export default function Home() {
           headline="Hi, I’m <b>Alex</b>"
           description="a <b>UI/UX Designer</b> based in <b>Los Angeles</b>. With over <b>3 years of experience</b>, I specialize in creating user-friendly digital products that solve real-world problems."
         />
-        <ArticleCard
-          title="About Me"
-          imgSrc="/img/about_me.jpg"
-          imgAlt="image of a girl to represent about me"
-          description="I’m a UI/UX designer with a passion for solving complex problems<br/>through intuitive design. My journey started 5 years ago, and since then, <br/>I've worked with startups and established brands to bring their visions to <br/>life. <br/><br/>Over the years, I've had the privilege of working with companies like <br/>ShopEase, FinTrack, and Insightly, focusing on user-centric design <br/>solutions."
-        />
+        <div className="flex flex-col md:flex-row-reverse gap-4">
+          <div className="flex md:w-1/4">
+            <Image
+              height={100}
+              width={200}
+              src="/img/about_me.jpg"
+              alt="image of a girl to represent about me"
+              className="object-cover object-top w-full h-[536px] md:h-[359px] rounded-2xl"
+            />
+          </div>
+          <ArticleCard
+            title="About Me"
+            description="I’m a UI/UX designer with a passion for solving complex problems<br/>through intuitive design. My journey started 5 years ago, and since then, <br/>I've worked with startups and established brands to bring their visions to <br/>life. <br/><br/>Over the years, I've had the privilege of working with companies like <br/>ShopEase, FinTrack, and Insightly, focusing on user-centric design <br/>solutions."
+          />
+        </div>
       </main>
     </div>
   );
